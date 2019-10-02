959574d4f464fd64c8a3798a0465a3dd30d63a76
Agronaut <agronaut001@gmail.com>
2019-10-02 12:46:19 +0300

    комментарий

---
+++ 1/make/hello.cpp
@@ -0,0 +1,14 @@
+/**
+  \file
+  \brief файл, содержащий функцию print_hello()
+
+  Функция выводит на экран Hello World!
+*/
+
+#include <iostream>
+#include "functions.h"
+
+using namespace std;
+void print_hello(){
+   cout << "Hello World!";
+}
---
+++ 1/make/main.cpp
@@ -0,0 +1,18 @@
+/**
+  \file
+  \brief Основная функция
+
+  Содержит основной код программы
+*/
+
+#include <iostream>
+#include "functions.h"
+
+using namespace std;
+
+int main(){
+    print_hello();
+    cout << endl;
+    cout << "The factorial of 5 is " << factorial(5) << endl;
+    return 0;
+}
